962b150b90edbaa92390bda02901745112c6571c
Etienne Poulin <etiennepoulin249@gmail.com>
2025-07-24 18:33:34 -0400

    Added better logging

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,20 @@ use dll_syringe::{
     Syringe,
 };
 use std::{
-    fs::read_to_string, os::windows::process::CommandExt, path::PathBuf, process::Command,
-    time::Duration,
+    fs::{read_to_string, File},
+    io::{BufRead, BufReader, Read, Write},
+    os::windows::process::CommandExt,
+    path::PathBuf,
+    process::{Command, Stdio},
+    thread,
+    time::{Duration, SystemTime, UNIX_EPOCH},
 };
 
 fn main() {
-    Command::new("../../Gw2-64.exe")
+    let mut f = File::create("log.txt").unwrap();
+    log(&mut f, "Launching gw2...");
+
+    let mut gw2_child = Command::new("../../Gw2-64.exe")
         .args(&[
             "-provider",
             "Portal",
@@ -19,10 +27,12 @@ fn main() {
         ])
         .creation_flags(0x08000000)
         .spawn()
-        .ok();
+        .unwrap();
 
+    log(&mut f, "Finding game process...");
     let process = OwnedProcess::find_first_by_name("Gw2-64").unwrap();
 
+    log(&mut f, "Waiting for user to launch the game...");
     //Quick and easy way to wait until the user has launched the game.
     if process
         .wait_for_module_by_name(
@@ -37,37 +47,73 @@ fn main() {
     //Just to make sure
     std::thread::sleep(Duration::from_secs(3));
 
+    log(&mut f, "Game has been launched.");
+
     let syringe = Syringe::for_process(process);
 
-    let dlls = get_dlls();
-    let exes = get_exes();
+    let dlls = get_dlls(&mut f);
+    let exes = get_exes(&mut f);
 
     for dll in dlls {
         if dll.as_os_str().len() > 3 {
-            inject_dll(&syringe, dll);
+            inject_dll(&syringe, dll, &mut f);
         }
     }
+
     for exe in exes {
         if exe.as_os_str().len() > 3 {
-            run_exe(exe);
+            let mut f_cloned = f.try_clone().unwrap();
+            thread::spawn(move || {
+                run_exe(exe, &mut f_cloned.try_clone().unwrap());
+            });
+        }
+    }
+
+    gw2_child.wait();
+}
+
+fn inject_dll(syringe: &Syringe, path: PathBuf, f: &mut File) {
+    match syringe.inject(&path) {
+        Ok(_) => log(
+            f,
+            &format!("Successfully injected {}", path.to_str().unwrap()),
+        ),
+        Err(e) => log(
+            f,
+            &format!(
+                "Failed injecting {}. Error: {}",
+                path.to_str().unwrap(),
+                e.to_string()
+            ),
+        ),
+    }
+}
+
+fn run_exe(path: PathBuf, f: &mut File) {
+    match Command::new(&path)
+        .creation_flags(0x08000000)
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .output()
+    {
+        Ok(mut child) => {
+            log(
+                f,
+                &format!("Successfully launched {}", path.to_str().unwrap()),
+            );
+        }
+        Err(e) => {
+            log(
+                f,
+                &format!("Failed to launch {}: {}", path.to_str().unwrap(), e),
+            );
         }
     }
 }
 
-fn inject_dll(syringe: &Syringe, path: PathBuf) {
-    match syringe.inject(path) {
-        Ok(_) => println!("success"),
-        Err(e) => println!("{}", &e.to_string()),
-    }
-}
-
-fn run_exe(path: PathBuf) {
-    Command::new(path).creation_flags(0x08000000).spawn().ok();
-}
-
-fn get_dlls() -> Vec<PathBuf> {
+fn get_dlls(f: &mut File) -> Vec<PathBuf> {
     if let Ok(contents) = read_to_string("dlls.txt") {
-        println!("READ THE FILE: {}", contents);
+        log(f, "Read dlls.");
         return contents
             .split(|b| b == '\n')
             .map(|line| PathBuf::from(line))
@@ -75,12 +121,26 @@ fn get_dlls() -> Vec<PathBuf> {
     }
     Vec::new()
 }
-fn get_exes() -> Vec<PathBuf> {
+fn get_exes(f: &mut File) -> Vec<PathBuf> {
     if let Ok(contents) = read_to_string("exes.txt") {
+        log(f, "Read exes.");
         return contents
             .split(|b| b == '\n')
             .map(|line| PathBuf::from(line))
             .collect::<Vec<PathBuf>>();
     }
     Vec::new()
+}
+
+fn log(f: &mut File, s: &str) {
+    println!("{}", s);
+    let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap();
+    f.write_all(
+        format!(
+            "[{}] [Gw2SimpleAddonLoader] {}\n",
+            format!("{}", now.as_secs()),
+            s
+        )
+        .as_bytes(),
+    );
 }
