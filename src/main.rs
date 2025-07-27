24ed702b1210f14b7cd3e74fa2f8d9a01d39df15
Etienne Poulin <etiennepoulin249@gmail.com>
2025-07-27 18:20:38 -0400

    Merge PR#1 - relative_paths

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,45 @@ use dll_syringe::{
     Syringe,
 };
 use std::{
+    env,
     fs::{read_to_string, File},
-    io::{BufRead, BufReader, Read, Write},
+    io::Write,
     os::windows::process::CommandExt,
-    path::{Path, PathBuf},
+    path::PathBuf,
     process::{Child, Command, Stdio},
-    thread,
     time::{Duration, SystemTime, UNIX_EPOCH},
 };
 
 fn main() {
-    let mut f = File::create("log.txt").unwrap();
-    log(&mut f, "Launching gw2...");
+    // Path to Gw2SimpleAddonLoader binary directory
+    let binary_path = PathBuf::from(env::current_exe().unwrap().parent().unwrap());
+    // Path to the log file
+    let mut log_path = binary_path.clone();
+    log_path.push("log.txt");
+    // Create the log file
+    let mut f = File::create(log_path.to_str().unwrap()).unwrap();
+    log(
+        &mut f,
+        &format!(
+            "Running from addon directory: \"{}\"",
+            binary_path.to_str().unwrap()
+        ),
+    );
 
+    // Guild Wars 2 binary should be 2 directories up
+    let mut gw2_path = binary_path.clone();
+    gw2_path.pop();
+    gw2_path.pop();
+    gw2_path.push("Gw2-64.exe");
+    log(
+        &mut f,
+        &format!(
+            "Looking for Gw2-64.exe on path: \"{}\"",
+            gw2_path.to_str().unwrap()
+        ),
+    );
+
+    log(&mut f, "Launching Guild Wars 2...");
     let mut gw2_child = Command::new("../../Gw2-64.exe")
         .args(&[
             "-provider",
@@ -29,10 +55,18 @@ fn main() {
         .spawn()
         .unwrap();
 
+    let dlls = get_dlls(&mut f, &binary_path);
+    let exes = get_exes(&mut f, &binary_path);
+
+    log(
+        &mut f,
+        &format!("Found {} dlls and {} exes", dlls.len(), exes.len()),
+    );
+
     log(&mut f, "Finding game process...");
     let process = OwnedProcess::find_first_by_name("Gw2-64").unwrap();
 
-    log(&mut f, "Waiting for user to launch the game...");
+    log(&mut f, "Waiting for game to be fully running...");
     //Quick and easy way to wait until the user has launched the game.
     process
         .wait_for_module_by_name("d3d11.dll", Duration::from_millis(u64::max_value()))
@@ -45,9 +79,6 @@ fn main() {
 
     let syringe = Syringe::for_process(process);
 
-    let dlls = get_dlls(&mut f);
-    let exes = get_exes(&mut f);
-
     for dll in dlls {
         if dll.as_os_str().len() > 3 {
             inject_dll(&syringe, dll, &mut f);
@@ -58,12 +89,12 @@ fn main() {
 
     for exe in exes {
         if exe.as_os_str().len() > 3 {
-            let mut f_cloned = f.try_clone().unwrap();
+            let f_cloned = f.try_clone().unwrap();
             children.push(run_exe(exe, &mut f_cloned.try_clone().unwrap()));
         }
     }
 
-    gw2_child.wait();
+    gw2_child.wait().ok();
 
     //Game is closed, gotta kill every exe launched to close the wine prefix/instance cleanly
     //For example, Blish like to stick around if not killed.
@@ -98,7 +129,7 @@ fn run_exe(path: PathBuf, f: &mut File) -> Option<Child> {
         .stderr(Stdio::piped())
         .spawn()
     {
-        Ok(mut child) => {
+        Ok(child) => {
             log(f, &format!("Launched {}", path.to_str().unwrap()));
             Some(child)
         }
@@ -112,9 +143,14 @@ fn run_exe(path: PathBuf, f: &mut File) -> Option<Child> {
     }
 }
 
-fn get_dlls(f: &mut File) -> Vec<PathBuf> {
-    if let Ok(contents) = read_to_string("dlls.txt") {
-        log(f, "Read dlls.");
+fn get_dlls(f: &mut File, binary_path: &PathBuf) -> Vec<PathBuf> {
+    let mut dlls_path = binary_path.clone();
+    dlls_path.push("dlls.txt");
+    log(
+        f,
+        &format!("Trying to read dlls from {}", dlls_path.to_str().unwrap()),
+    );
+    if let Ok(contents) = read_to_string(dlls_path) {
         return contents
             .split(|b| b == '\n')
             .map(|line| PathBuf::from(line))
@@ -122,9 +158,15 @@ fn get_dlls(f: &mut File) -> Vec<PathBuf> {
     }
     Vec::new()
 }
-fn get_exes(f: &mut File) -> Vec<PathBuf> {
-    if let Ok(contents) = read_to_string("exes.txt") {
-        log(f, "Read exes.");
+
+fn get_exes(f: &mut File, binary_path: &PathBuf) -> Vec<PathBuf> {
+    let mut exes_path = binary_path.clone();
+    exes_path.push("exes.txt");
+    log(
+        f,
+        &format!("Trying to read exes from {}", exes_path.to_str().unwrap()),
+    );
+    if let Ok(contents) = read_to_string(exes_path) {
         return contents
             .split(|b| b == '\n')
             .map(|line| PathBuf::from(line))
@@ -143,5 +185,6 @@ fn log(f: &mut File, s: &str) {
             s
         )
         .as_bytes(),
-    );
+    )
+    .ok();
 }
